daba8b3948a892594778cf170ef5683e9a2dfdbc
Lakoji Harshit <139139453+HarshitLakoji@users.noreply.github.com>
2025-09-04 21:35:52 +0530

    Add files via upload

---
+++ AuthForm.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+export default function AuthForm() {
+  const [isLogin, setLogin] = useState(true);
+  return (
+    <div className="container">
+      <div className="form-container">
+        <div className="form-toggle">
+          <button
+            className={isLogin ? "active" : ""}
+            onClick={() => {setLogin(true);}}
+          >
+            Login
+          </button>
+          <button
+            className={!isLogin ? "active" : ""}
+            onClick={() =>{ setLogin(false);}}
+          >
+            Sign Up
+          </button>
+        </div>
+        {isLogin ? (
+          <>
+            <div className="form">
+              <h2>Login Form</h2>
+              <input type="email" placeholder="Email" />
+              <input type="password" placeholder="Password" />
+              <a href="#">Forget Password</a>
+              <button>Login</button>
+              <p>
+                Not a member? {" "}
+                <a href="#" onClick={()=>{setLogin(false);}}>
+                  Signup Now
+                </a>
+              </p>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="form">
+              <h2>Signup Form</h2>
+              <input type="email" placeholder="Email" />
+              <input type="password" placeholder="Password" />
+              <input type="password" placeholder="confirm password" />
+              <button>Signup</button>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
